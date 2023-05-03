1a59fdb8affd275a0c8fbc5b70555d2a8a93c6b6
amiminn <tholabul.amin@gmail.com>
2023-05-03 22:34:05 +0700

    x-model

---
+++ js/async.js
@@ -0,0 +1,28 @@
+// sync -> berurutan dari atas ke bawah. 5sec
+
+function nama() {
+  console.log("ilham");
+}
+
+function sekolah() {
+  console.log("ke sekolah smp");
+}
+
+// nama();
+// sekolah();
+
+// async 1 sec
+
+// nama();
+
+// setTimeout(() => {
+//   console.log("makan");
+// }, 3000);
+
+// sekolah();
+
+// type async
+
+// promise new Promise()
+// then catch
+// async await
